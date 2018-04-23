61af148d8f17a19c1b2964e175dbda20cf8b705a
Terrence <30850978+TerrenceGao@users.noreply.github.com>
2018-04-23 19:31:46 -0400

    to ex31

---
+++ ex266.py
@@ -0,0 +1,22 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
